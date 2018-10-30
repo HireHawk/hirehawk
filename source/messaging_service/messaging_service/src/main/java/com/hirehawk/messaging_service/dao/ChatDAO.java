6bc5d4082b5b3cc35810ea6c2372be69629d44b1
Maxim <maxim1998g@gmail.com>
2018-10-30 10:09:10 +0200

    Implement main chat queries logic

--- source/messaging_service/messaging_service/src/main/java/com/hirehawk/messaging_service/dao/ChatDAO.java
+++ source/messaging_service/messaging_service/src/main/java/com/hirehawk/messaging_service/dao/ChatDAO.java
@@ -2,18 +2,59 @@ package com.hirehawk.messaging_service.dao;
 
 import com.hirehawk.messaging_service.entity.Chat;
 import com.hirehawk.messaging_service.entity.ChatMediaFile;
+import com.hirehawk.messaging_service.entity.ChatMessage;
+import com.hirehawk.messaging_service.entity.ChatUser;
 
 import java.util.Date;
+import java.util.List;
 
 public interface ChatDAO {
 
-    public void addChat(String name, int icon, String additionalInfo);
+    void addChat(String name, int icon, String additionalInfo);
 
-    public void addMediaFile(String filename, String extention, byte[] media, String mimetype, byte[] thumbnail);
+    void addMediaFile(String filename, String extention, byte[] media, String mimetype, byte[] thumbnail);
 
-    public void addMessage(int author, int receiver, int chat, String text, int mediaFile, Date stamp, Date editStamp, int deleted);
+    void addMessage(int author, int receiver, int chat, String text, int mediaFile, Date stamp, Date editStamp, int deleted);
 
-    public Chat getChatById(int id);
+    Chat getChatById(int id);
 
-    public ChatMediaFile getMediaFileById(int id);
+    ChatMediaFile getMediaFileById(int id);
+
+    ChatMessage getMessageById(int id);
+
+    void editMessage(int id, String text, Date stamp);
+
+    void deleteMessage(int id);
+
+    void userDeleteMessage(int id, Date stamp);
+
+    void userUndoDeleteMessage(int id, Date stamp);
+
+    List<ChatMessage> getAllUserConversationMessages(int userId, int chatId);
+
+    List<ChatMessage> getAllUserChatMessages(int userId, int chatId);
+
+    void setNewUnreadMessage(int messageId, int authorId, int receiverId, int chatId);
+
+    void setMessagesAsReaded(int receiverId, int authorId, int chatId, int lastId);
+
+    void setDialog(int firstId, int secondId);
+
+    List<ChatUser> getChatUsers(int chatId);
+
+    void addChatUser(int chatId, int userId);
+
+    void removeChatUser(int chatId, int userId);
+
+    List<ChatMessage> getAllUnreadMessages(int userId);
+
+    List getAllUnreadMessagesIds(int userId);
+
+    List<ChatMessage> getAllUserUnreadMessages(int userId, int chatId);
+
+    List<Object> getAllUserConversations(int userId);
+
+    boolean checkParticipate(int userId, int dialogId, int chatId);
+
+    boolean hasNewChats(int userId);
 }
--- source/messaging_service/messaging_service/src/main/java/com/hirehawk/messaging_service/dao/ChatDAOImpl.java
+++ source/messaging_service/messaging_service/src/main/java/com/hirehawk/messaging_service/dao/ChatDAOImpl.java
@@ -4,6 +4,7 @@ package com.hirehawk.messaging_service.dao;
 import com.hirehawk.messaging_service.entity.Chat;
 import com.hirehawk.messaging_service.entity.ChatMediaFile;
 import com.hirehawk.messaging_service.entity.ChatMessage;
+import com.hirehawk.messaging_service.entity.ChatMessageStatus;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
@@ -11,8 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Transactional
 @Repository
@@ -76,5 +78,293 @@ public class ChatDAOImpl implements ChatDAO {
     public ChatMediaFile getMediaFileById(int id) {
         Session session = this.sessionFactory.getCurrentSession();
         return session.find(ChatMediaFile.class, id);
+    }
+
+    @Override
+    public  ChatMessage getMessageById(int id) {
+        Session session = this.sessionFactory.getCurrentSession();
+        return session.find(ChatMessage.class, id);
+    }
+
+    @Override
+    public void editMessage(int id, String text, Date stamp) {
+        Session session = this.sessionFactory.getCurrentSession();
+        ChatMessage oldMess = this.getMessageById(id);
+        oldMess.setText(text);
+        oldMess.setEditStamp(stamp);
+
+        session.update(oldMess);
+        session.flush();
+    }
+
+    @Override
+    public void deleteMessage(int id) {
+        Session session = this.sessionFactory.getCurrentSession();
+        ChatMessage mess = this.getMessageById(id);
+
+        session.delete(mess);
+        session.flush();
+    }
+
+    @Override
+    public void userDeleteMessage(int id, Date stamp) {
+        Session session = this.sessionFactory.getCurrentSession();
+        ChatMessage mess = this.getMessageById(id);
+        mess.setDeleted(1);
+        mess.setEditStamp(stamp);
+
+        session.update(mess);
+        session.flush();
+    }
+
+    @Override
+    public void userUndoDeleteMessage(int id, Date stamp) {
+        Session session = this.sessionFactory.getCurrentSession();
+        ChatMessage mess = this.getMessageById(id);
+        mess.setDeleted(0);
+        mess.setEditStamp(stamp);
+
+        session.update(mess);
+        session.flush();
+    }
+
+    @Override
+    public List getAllUserConversationMessages(int userId, int chatId) {
+        Session session = this.sessionFactory.getCurrentSession();
+
+        String sql = "FROM ChatMessage cm " +
+                "WHERE (cm.reciever_id = :userId AND cm.author_id = :chatId) " +
+                    "OR (cm.reciever_id = :chatId AND cm.author_id = :userId)";
+        Query query = session.createQuery(sql)
+                .setParameter("userId", userId)
+                .setParameter("chatId", chatId);
+
+        return query.list();
+    }
+
+    @Override
+    public List getAllUserChatMessages(int userId, int chatId) {
+        Session session = this.sessionFactory.getCurrentSession();
+
+        String sqlUpd = "UPDATE ChatUser SET isNew = 0 " +
+                "WHERE chat_id = :chatId AND user_id = :userId";
+        Query queryUpd = session.createQuery(sqlUpd)
+                .setParameter("chatId", chatId)
+                .setParameter("userId", userId);
+        int result = queryUpd.executeUpdate();
+
+        if (result != 1) {
+            System.out.println("Incorrect update in getAllUserChatMessages, result: " + result);
+        }
+
+        String sql = "FROM ChatMessage cm WHERE cm.chat_id = :chatId";
+        Query query = session.createQuery(sql)
+                .setParameter("chatId", chatId);
+
+        return query.list();
+    }
+
+    @Override
+    public void setNewUnreadMessage(int messageId, int authorId, int receiverId, int chatId) {
+        ChatMessageStatus status = new ChatMessageStatus();
+        status.setMessage_id(messageId);
+        status.setAuthor_id(authorId);
+        status.setReciever_id(receiverId);
+        status.setChat_id(chatId);
+
+        Session session = this.sessionFactory.getCurrentSession();
+        session.persist(status);
+        session.flush();
+    }
+
+    @Override
+    public void setMessagesAsReaded(int receiverId, int authorId, int chatId, int lastId) {
+        Session session = this.sessionFactory.getCurrentSession();
+
+        String sql = "DELETE ChatMessageStatus " +
+                "WHERE reciever_id = :receiverId " +
+                "AND (author_id = :authorId OR chat_id = :chatId) AND message_id <= :lastId;"
+
+        Query query = session.createQuery(sql)
+                .setParameter("lastId", lastId)
+                .setParameter("chatId", chatId)
+                .setParameter("authorId", authorId);
+
+        query.executeUpdate();
+    }
+
+    @Override
+    public void setDialog(int firstId, int secondId) {
+        Session session = this.sessionFactory.getCurrentSession();
+
+        String sql = "INSERT INTO ChatUser(user_id, private_user)" +
+                "SELECT b.v1, b.v2 " +
+                "FROM " +
+                    "(SELECT IF(a.num = 0, $firstId, NULL) AS v1, IF(a.num = 0, $secondId, NULL) AS v2 " +
+                    "FROM " +
+                        "(SELECT COUNT(*) AS num " +
+                        "FROM chats_users " +
+                        "WHERE (chat_id = -1 AND ((user_id = $firstId AND private_user = $secondId) " +
+                            "OR (user_id = $secondId AND private_user = $firstId)))" +
+                        ") AS a" +
+                    ") AS b " +
+                "WHERE (b.v1 IS NOT NULL) AND (b.v2 IS NOT NULL)";
+        Query query = session.createQuery(sql)
+                .setParameter("firstId", firstId)
+                .setParameter("secondId", secondId);
+
+        query.executeUpdate();
+    }
+
+    @Override
+    public List getChatUsers(int chatId) {
+        Session session = this.sessionFactory.getCurrentSession();
+
+        String sql = "FROM ChatUser WHERE chat_id = :chatId";
+
+        Query query = session.createQuery(sql)
+                .setParameter("chatId", chatId);
+
+        return query.list();
+    }
+
+    @Override
+    public void addChatUser(int chatId, int userId) {
+        Session session = this.sessionFactory.getCurrentSession();
+
+        String sql = "INSERT INTO ChatUser (chat_id, user_id) " +
+                "SELECT b.v1, b.v2 " +
+                "FROM " +
+                    "(SELECT IF(a.num = 0, :chatId, NULL) AS v1, IF(a.num = 0, :userId, NULL) AS v2 " +
+                    "FROM " +
+                        "(SELECT COUNT(*) AS num " +
+                        "FROM ChatUsers " +
+                        "WHERE (chat_id = :chatId AND user_id = :userId)" +
+                        ") AS a" +
+                    ") AS b " +
+                "WHERE (b.v1 IS NOT NULL) AND (b.v2 IS NOT NULL)";
+
+        Query query = session.createQuery(sql)
+                .setParameter("chatId", chatId)
+                .setParameter("userId", userId);
+
+        query.executeUpdate();
+    }
+
+    @Override
+    public void removeChatUser(int chatId, int userId) {
+        Session session = this.sessionFactory.getCurrentSession();
+
+        String sql = "DELETE FROM ChatUser " +
+                "WHERE chat_id = :chatId AND user_id = :userId";
+
+        Query query = session.createQuery(sql)
+                .setParameter("chatId", chatId)
+                .setParameter("userId", userId);
+
+        query.executeUpdate();
+    }
+
+    @Override
+    public List getAllUnreadMessages(int userId) {
+        Session session = this.sessionFactory.getCurrentSession();
+
+        String sql = "FROM ChatMessage cm " +
+                "INNER JOIN ChatMessageStatus " +
+                "WHERE message_id = cm.id" +
+                        "AND reciever_id = :userId)";
+
+        Query query = session.createQuery(sql)
+                .setParameter("userId", userId);
+
+        return query.list();
+    }
+
+    @Override
+    public List getAllUnreadMessagesIds(int userId) {
+        Session session = this.sessionFactory.getCurrentSession();
+
+        String sql = "SELECT cm.id " +
+                "FROM ChatMessage cm " +
+                    "INNER JOIN ChatMessageStatus as cms " +
+                    "WHERE cms.message_id = cm.id AND cms.reciever_id = :userId";
+
+        Query query = session.createQuery(sql)
+                .setParameter("userId", userId);
+
+        return query.list();
+    }
+
+    @Override
+    public List getAllUserUnreadMessages(int userId, int chatId) {
+        Session session = this.sessionFactory.getCurrentSession();
+
+        String sql = "FROM ChatMessage cm " +
+                "INNER JOIN ChatMessageStatus " +
+                "WHERE message_id = cm.id AND reciever_id = :userId " +
+                    "AND author_id = :chatId)";
+
+        Query query = session.createQuery(sql)
+                .setParameter("chatId", chatId)
+                .setParameter("userId", userId);
+
+        return query.list();
+    }
+
+    @Override
+    public List<Object> getAllUserConversations(int userId) {
+        Session session = this.sessionFactory.getCurrentSession();
+
+        String sqlChat = "SELECT cu.chat_id " +
+                "FROM ChatUser cu " +
+                "WHERE cu.user_id = :userId AND cu.chat_id <> -1";
+        String sqlDialog = "SELECT IF(cu.user_id = :userId, cu.private_user, cu.user_id) AS private " +
+                "FROM ChatUser cu " +
+                "WHERE cu.chat_id = -1 AND (cu.user_id = :userId OR cu.private_user = :userId)";
+
+        Query queryChat = session.createQuery(sqlChat)
+                .setParameter("userId", userId);
+
+        Query queryDialog = session.createQuery(sqlDialog)
+                .setParameter("userId", userId);
+
+
+        // TODO: separate
+        List<Object> both = new ArrayList<Object>(queryChat.list());
+        both.addAll(queryDialog.list());
+        return both;
+    }
+
+    @Override
+    public boolean checkParticipate(int userId, int dialogId, int chatId) {
+        Session session = this.sessionFactory.getCurrentSession();
+
+        String sql = "SELECT COUNT(cu) " +
+                "FROM ChatUser cu " +
+                "WHERE cu.chat_id = :chatId " +
+                    "AND ((cu.user_id = :userId AND cu.private_user = :dialogId) " +
+                    "OR (cu.chat_id = -1 AND cu.user_id = :dialogId AND cu.private_user = :userId))";
+
+        Query query = session.createQuery(sql)
+                .setParameter("chatId", chatId)
+                .setParameter("userId", userId)
+                .setParameter("dialogId", dialogId);
+
+        Long count = (Long)query.uniqueResult();
+        return count > 0;
+    }
+
+    @Override
+    public boolean hasNewChats(int userId) {
+        Session session = this.sessionFactory.getCurrentSession();
+
+        String sql = "SELECT count(cu) FROM ChatUser cu " +
+                "WHERE cu.isNew = 1 AND cu.user_id = :userId";
+
+        Query query = session.createQuery(sql)
+                .setParameter("userId", userId);
+
+        Long count = (Long)query.uniqueResult();
+        return count != 0;
     }
 }
