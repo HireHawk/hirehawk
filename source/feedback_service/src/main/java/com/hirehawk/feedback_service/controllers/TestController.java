aa76d0e6e410af5914eb67d21bd97bc6ef3c3d59
UlozhenkoMarina <marina.ulozhenko@gmail.com>
2018-11-19 22:59:54 +0200

    Add files via upload

--- source/feedback_service/src/main/java/com/hirehawk/feedback_service/controllers/TestController.java
+++ source/feedback_service/src/main/java/com/hirehawk/feedback_service/controllers/TestController.java
@@ -1,49 +1,83 @@
-package com.hirehawk.feedback_service.controllers;
+package com.hirehawk.feedback.controllers;
 
-import com.hirehawk.feedback_service.dao.FeedbackDAO;
-import com.hirehawk.feedback_service.dao.USERROLE;
-import com.hirehawk.feedback_service.entity.Feedback;
-import org.json.simple.parser.JSONParser;
+import com.hirehawk.feedback.dao.FeedbackDAO;
+import com.hirehawk.feedback.dao.USERROLE;
+import com.hirehawk.feedback.entities.Feedback;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
-import org.json.simple.*;
 
 import java.util.Date;
 import java.util.List;
 
-
-@Controller
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
 @Transactional
 public class TestController {
+
 
     @Autowired
     private FeedbackDAO feedbackDAO;
 
-    @RequestMapping("/")
-    public RedirectView home() {
-        feedbackDAO.saveFeedback(4, "some comment", new Date(2018,10,8,18,28,20),
-                1, 2, USERROLE.GIVER);
-        feedbackDAO.saveFeedback(3, new Date(2018,10,8,17,10,20),
-                1, 2, USERROLE.GIVER);
-        feedbackDAO.saveFeedback(2, "some another comment", new Date(2018,10,8,18,10,20),
-                1, 2, USERROLE.GIVER);
 
-        List<Feedback> feedbacks = feedbackDAO.getFeedbacks(2, USERROLE.GIVER);
-        System.out.println(feedbacks.size());
-        feedbacks = feedbackDAO.getFeedbacksWithoutComments(2, USERROLE.GIVER);
-        System.out.println(feedbacks.size());
-        feedbacks = feedbackDAO.getFeedbacksWithComments(2, USERROLE.GIVER);
-        System.out.println(feedbacks.size());
-        // return "index";
-        return new RedirectView("feedback");
+    @RequestMapping(value = "/save_with_comment", method = RequestMethod.POST)
+    public void create( int mark, String comment, Date datetime, Integer userWhoLeft,  Integer userAbout, USERROLE userAboutRole)
+    {
+        feedbackDAO.saveFeedback(mark, comment, datetime, userWhoLeft, userAbout, userAboutRole);
     }
 
+    @RequestMapping(value = "/save_without_comment", method = RequestMethod.POST)
+    public void create_with_comment( int mark, Date datetime, Integer userWhoLeft,  Integer userAbout, USERROLE userAboutRole)
+    {
+        feedbackDAO.saveFeedback(mark, datetime, userWhoLeft, userAbout, userAboutRole);
+    }
+
+    @RequestMapping(value="/get_feedbacks", method=RequestMethod.GET)
+    public List<Feedback> get_feedbacks(int user, USERROLE role)
+    {
+        return feedbackDAO.getFeedbacks(user,role);
+    }
+
+
+
+
+    @RequestMapping(value="/get_feedbacks_n", method=RequestMethod.GET)
+    public List<Feedback> get_feedbacks_n(int user,USERROLE role,int num)
+    {
+        return feedbackDAO.getFeedbacks(user,role,num);
+    }
+
+
+
+    @RequestMapping(value="/get_feedbacks_without_comments", method=RequestMethod.GET)
+    public List<Feedback> get_feedbacks_without_comments( int user,  USERROLE role)
+    {
+        return feedbackDAO.getFeedbacksWithoutComments(user, role);
+    }
+
+
+    @RequestMapping(value="/get_feedbacks_without_comments_n", method=RequestMethod.GET)
+    public List<Feedback> get_feedbacks_without_comments_n( int user,  USERROLE role,  int num)
+    {
+        return feedbackDAO.getFeedbacksWithoutComments(user,role,num);
+    }
+
+    @RequestMapping(value="/get_feedbacks_with_comments",method=RequestMethod.GET)
+    public List<Feedback> get_feedbacks_with_comments(int user,USERROLE role)
+    {
+        return feedbackDAO.getFeedbacksWithComments(user,role);
+    }
+
+    @RequestMapping(value="/get_feedbacks_with_comments_n",method=RequestMethod.GET)
+    public List<Feedback> get_feedbacks_with_comments_n(int user,USERROLE role, int num)
+    {
+        return feedbackDAO.getFeedbacksWithComments(user,role,num);
+    }
+
+
+
+/*
     @GetMapping("/feedback")
     public String feedback() {
         feedbackDAO.saveFeedback(4, "some comment", new Date(2018,10,8,18,28,20),
@@ -54,20 +88,18 @@ public class TestController {
     @PostMapping("/fs1")
     public String g()
             /*(@PathVariable int mark, @PathVariable String comm, @PathVariable Date data,
-                    @PathVariable Integer who, @PathVariable Integer about, @PathVariable String role)*/ {
-                        System.out.println("cgjfjgk");
-       //  System.out.println(smth);
+                    @PathVariable Integer who, @PathVariable Integer about, @PathVariable String role) {
+        System.out.println("cgjfjgk");
+        //  System.out.println(smth);
         USERROLE role_;
 
         /*System.out.println(mark+comm+data+who+about+role);
             feedbackDAO.saveFeedback(mark, comm, data,
                     who, about, USERROLE.BOTH);
-            System.out.println("saved");*/
+            System.out.println("saved");
 
         //JsonObject jsonObject = new JSONParser().parse(json).getAsJsonObject();
 
         return "index";
-    }
+    }*/
 }
-
-
