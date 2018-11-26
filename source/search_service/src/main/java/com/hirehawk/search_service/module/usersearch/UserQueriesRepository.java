6837d83422213df78459ccbeb96b154f6751cf25
VolfP <pavel98volk@gmail.com>
2018-11-26 23:07:04 +0200

    Death to search service!

--- source/search_service/src/main/java/com/hirehawk/search_service/module/usersearch/UserQueriesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hirehawk.search_service.module.usersearch;
-
-import com.hirehawk.search_service.models.User;
-
-import java.util.List;
-
-public interface UserQueriesRepository {
-    public List<User> search(String searchTerm);
-}
--- source/search_service/src/main/java/com/hirehawk/search_service/module/usersearch/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hirehawk.search_service.module.usersearch;
-
-import com.hirehawk.search_service.models.User;
-import org.springframework.data.solr.repository.SolrCrudRepository;
-
-public interface UserRepository extends UserQueriesRepository, SolrCrudRepository<User, String> {
-}
