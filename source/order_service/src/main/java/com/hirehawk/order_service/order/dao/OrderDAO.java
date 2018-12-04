785c2af252a29b6d7cad3af9fb810ee5c4cccd29
UlozhenkoMarina <marina.ulozhenko@gmail.com>
2018-12-04 09:05:53 +0200

    Add files via upload

--- source/order_service/src/main/java/com/hirehawk/order_service/order/dao/OrderDAO.java
+++ source/order_service/src/main/java/com/hirehawk/order_service/order/dao/OrderDAO.java
@@ -2,6 +2,7 @@ package com.hirehawk.order_service.order.dao;
 
 import com.hirehawk.order_service.order.entities.Order;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.util.Date;
 import java.util.List;
 
@@ -9,17 +10,23 @@ import java.util.List;
 public interface OrderDAO {
 
     //public void save_order(String advert, String userWhoGive,String userWhoGet,Date date,int payment);
-    public void save_order_without_payment(String advert, String userWhoGive,String userWhoGet,Date date,Date start, Date finish,float price);
-    public void save_order_with_payment(String advert, String userWhoGive,String userWhoGet,Date date,int payment,Date start, Date finish,float price);
-   public void update_istransfer(Integer id,String advert, String userWhoGive,String userWhoGet,Date date,int payment,boolean isreturn,boolean istransfer,Date start,Date finish,float price,boolean istnew);
-   public void  update_isreturn(Integer id,String advert, String userWhoGive,String userWhoGet,Date date,int payment,boolean isreturn,boolean istransfer,Date start,Date finish,float price,boolean isrnew);
+    public void save_order_without_payment(String advert, String userWhoGive,String userWhoGet,Date date,Date start, Date finish,Float price);
+    //public void save_order_with_payment(String advert, String userWhoGive,String userWhoGet,Date date,int payment,Date start, Date finish,float price);
+   public void update_istransfer(Integer id,String advert, String userWhoGive,String userWhoGet,Date date,Boolean payment,Boolean isreturn,Boolean istransfer,Date start,Date finish,Float price,Boolean istnew);
+   public void  update_isreturn(Integer id,String advert, String userWhoGive,String userWhoGet,Date date,Boolean payment,Boolean isreturn,Boolean istransfer,Date start,Date finish,Float price,Boolean isrnew);
    public void del(Integer id);
    public List<Order> show_all_give(String userWhoGive);
     public List<Order> show_all_get(String userWhoGet);
-    public List<Order> show_n_give(String userWhoGive,int num);
-    public List<Order> show_n_get(String userWhoGet,int num);
+    public List<Order> show_n_give(String userWhoGive,Integer num);
+    public List<Order> show_n_get(String userWhoGet,Integer num);
    public List<Order> show_depend_date_give(String userWhoGive,Date date);
     public List<Order> show_depend_date_get(String userWhoGet,Date date);
      public List<Order> show_not_isreturn(String userWhoGive);
     //public List<Order> show_on_istransfer(String userWhoGet);
+    public float getPrices(Integer id);
+    public void update_payment(Integer id,String advert, String userWhoGive,String userWhoGet,Date date,Boolean payment,Boolean isreturn,Boolean istransfer,Date start,Date finish,Float price,Boolean new_payment);
+    public Order show_order(Integer id);
+    public String show_userWhoGet(Integer id);
+    public String show_userWhoGive(Integer id);
+    public void update_giveagree(Integer id,String advert, String userWhoGive,String userWhoGet,Date date,Boolean payment,Boolean isreturn,Boolean istransfer,Date start,Date finish,Float price,Boolean newgiveagree);
 }
--- source/order_service/src/main/java/com/hirehawk/order_service/order/dao/OrderDAOImpl.java
+++ source/order_service/src/main/java/com/hirehawk/order_service/order/dao/OrderDAOImpl.java
@@ -21,7 +21,7 @@ public class OrderDAOImpl implements OrderDAO {
     private SessionFactory sessionFactory;
 
     @Override
-    public void save_order_without_payment(String advert, String userWhoGive,String userWhoGet,Date date,Date start, Date finish,float price){
+    public void save_order_without_payment(String advert, String userWhoGive,String userWhoGet,Date date,Date start, Date finish,Float price){
         Order order = new Order();
         order.setAdvert(advert);
         order.setUserWhoGet(userWhoGet);
@@ -29,6 +29,7 @@ public class OrderDAOImpl implements OrderDAO {
         order.setDate(date);
         order.setIsreturn(false);
         order.setIstransfer(false);
+        order.setPayment(false);
         order.setStart(start);
         order.setFinish(finish);
         order.setPrice(price);
@@ -37,7 +38,7 @@ public class OrderDAOImpl implements OrderDAO {
         session.flush();
     }
 
-    @Override
+   /* @Override
     public void save_order_with_payment(String advert, String userWhoGive,String userWhoGet,Date date,int payment,Date start, Date finish,float price){
         Order order = new Order();
         order.setAdvert(advert);
@@ -53,10 +54,10 @@ public class OrderDAOImpl implements OrderDAO {
         Session session=this.sessionFactory.getCurrentSession();
         session.persist(order);
         session.flush();
-    }
+    }*/
 
     @Override
-    public void update_istransfer(Integer id,String advert, String userWhoGive,String userWhoGet,Date date,int payment,boolean isreturn,boolean istransfer,Date start,Date finish,float price,boolean istnew){
+    public void update_istransfer(Integer id,String advert, String userWhoGive,String userWhoGet,Date date,Boolean payment,Boolean isreturn,Boolean istransfer,Date start,Date finish,Float price,Boolean istnew){
         Session session=this.sessionFactory.getCurrentSession();
         Order order=new Order();
         order.setId(id);
@@ -76,7 +77,7 @@ public class OrderDAOImpl implements OrderDAO {
     }
 
     @Override
-    public void update_isreturn(Integer id,String advert, String userWhoGive,String userWhoGet,Date date,int payment,boolean isreturn,boolean istransfer,Date start,Date finish,float price,boolean isrnew){
+    public void update_isreturn(Integer id,String advert, String userWhoGive,String userWhoGet,Date date,Boolean payment,Boolean isreturn,Boolean istransfer,Date start,Date finish,Float price,Boolean isrnew){
         Session session=this.sessionFactory.getCurrentSession();
         Order order=new Order();
         order.setId(id);
@@ -94,6 +95,27 @@ public class OrderDAOImpl implements OrderDAO {
         order.setIsreturn(isrnew);
         session.update(order);
     }
+
+
+    @Override
+    public void update_giveagree(Integer id,String advert, String userWhoGive,String userWhoGet,Date date,Boolean payment,Boolean isreturn,Boolean istransfer,Date start,Date finish,Float price,Boolean newgiveagree){
+        Session session=this.sessionFactory.getCurrentSession();
+        Order order=new Order();
+        order.setId(id);
+        order.setAdvert(advert);
+        order.setUserWhoGive(userWhoGive);
+        order.setUserWhoGet(userWhoGet);
+        order.setDate(date);
+        order.setPayment(payment);
+        order.setIsreturn(isreturn);
+        order.setIstransfer(istransfer);
+        order.setStart(start);
+        order.setFinish(finish);
+        order.setPrice(price);
+        session.evict(order);
+        order.setIsreturn(newgiveagree);
+        session.update(order);
+     }
 
     @Override
     public void del(Integer id){
@@ -127,7 +149,7 @@ public class OrderDAOImpl implements OrderDAO {
     }
 
     @Override
-    public List<Order> show_n_give(String userWhoGive,int num){
+    public List<Order> show_n_give(String userWhoGive,Integer num){
         List<Order> result = show_all_give(userWhoGive);
         result.sort(new DateComparator());
         if (num < result.size()) {
@@ -138,7 +160,7 @@ public class OrderDAOImpl implements OrderDAO {
 
 
     @Override
-    public List<Order> show_n_get(String userWhoGet,int num){
+    public List<Order> show_n_get(String userWhoGet,Integer num){
         List<Order> result = show_all_get(userWhoGet);
         result.sort(new DateComparator());
         if (num < result.size()) {
@@ -187,6 +209,69 @@ public class OrderDAOImpl implements OrderDAO {
                 result.add(result_all.get(i));
         }
         return result;
+    }
+
+    @Override
+    public float getPrices(Integer id){
+        Session session = this.sessionFactory.getCurrentSession();
+        String sql = "SELECT f FROM " + Order.class.getName() + " f WHERE f.id =:id";
+        Query<Order> query = session.createQuery(sql, Order.class);
+        query.setParameter("id", id);
+        List<Order> res = query.getResultList();
+        return res.get(0).getPrice();
+    }
+
+
+    @Override
+    public String show_userWhoGet(Integer id){
+        Session session = this.sessionFactory.getCurrentSession();
+        String sql = "SELECT f FROM " + Order.class.getName() + " f WHERE f.id =:id";
+        Query<Order> query = session.createQuery(sql, Order.class);
+        query.setParameter("id", id);
+        List<Order> res = query.getResultList();
+        return res.get(0).getUserWhoGet();
+    }
+
+    @Override
+    public Order show_order(Integer id){
+        Session session = this.sessionFactory.getCurrentSession();
+        String sql = "SELECT f FROM " + Order.class.getName() + " f WHERE f.id =:id";
+        Query<Order> query = session.createQuery(sql, Order.class);
+        query.setParameter("id", id);
+        List<Order> res = query.getResultList();
+        return res.get(0);
+    }
+
+
+    @Override
+    public String show_userWhoGive(Integer id){
+        Session session = this.sessionFactory.getCurrentSession();
+        String sql = "SELECT f FROM " + Order.class.getName() + " f WHERE f.id =:id";
+        Query<Order> query = session.createQuery(sql, Order.class);
+        query.setParameter("id", id);
+        List<Order> res = query.getResultList();
+        return res.get(0).getUserWhoGive();
+    }
+
+
+    @Override
+    public void update_payment(Integer id,String advert, String userWhoGive,String userWhoGet,Date date,Boolean payment,Boolean isreturn,Boolean istransfer,Date start,Date finish,Float price,Boolean new_payment){
+        Session session=this.sessionFactory.getCurrentSession();
+        Order order=new Order();
+        order.setId(id);
+        order.setAdvert(advert);
+        order.setUserWhoGive(userWhoGive);
+        order.setUserWhoGet(userWhoGet);
+        order.setDate(date);
+        order.setPayment(payment);
+        order.setIsreturn(isreturn);
+        order.setIstransfer(istransfer);
+        order.setStart(start);
+        order.setFinish(finish);
+        order.setPrice(price);
+        session.evict(order);
+        order.setPayment(new_payment);
+        session.update(order);
     }
 
 
