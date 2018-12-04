06284845bcf545640388bd0bbc79ebc04216b534
UlozhenkoMarina <marina.ulozhenko@gmail.com>
2018-12-04 09:04:59 +0200

    Add files via upload

--- source/feedback_service/src/main/java/com/hirehawk/feedback_service/feedback/dao/FeedbackDAO.java
+++ source/feedback_service/src/main/java/com/hirehawk/feedback_service/feedback/dao/FeedbackDAO.java
@@ -2,24 +2,39 @@ package com.hirehawk.feedback_service.feedback.dao;
 
 import com.hirehawk.feedback_service.feedback.entities.Feedback;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.util.Date;
 import java.util.List;
 
 public interface FeedbackDAO {
 
-    public void saveFeedback(int mark, String comment, Date datetime, String userWhoLeft, String userAbout, USERROLE userAboutRole);
+    public void saveFeedback(Integer mark, String comment, Date datetime, String userWhoLeft, String userAbout, USERROLE userAboutRole,String advert);
 
-    public void saveFeedback(int mark, Date datetime, String userWhoLeft, String userAbout, USERROLE userAboutRole);
+    public void saveFeedback(Integer mark, Date datetime, String userWhoLeft, String userAbout, USERROLE userAboutRole,String advert);
 
     public List<Feedback> getFeedbacks(String user, USERROLE role);
 
-    public List<Feedback> getFeedbacks(String user, USERROLE role, int num);
+    public List<Feedback> getFeedbacks(String user, USERROLE role, Integer num);
 
     public List<Feedback> getFeedbacksWithoutComments(String user, USERROLE role);
 
-    public List<Feedback> getFeedbacksWithoutComments(String user, USERROLE role, int num);
+    public List<Feedback> getFeedbacksWithoutComments(String user, USERROLE role, Integer num);
 
     public List<Feedback> getFeedbacksWithComments(String user, USERROLE role);
 
-    public List<Feedback> getFeedbacksWithComments(String user, USERROLE role, int num);
+    public List<Feedback> getFeedbacksWithComments(String user, USERROLE role, Integer num);
+
+    public void delFeedback(Integer id);
+
+    public List<Feedback> getFeedbacksByAdvert(String advert_id,Integer num);
+
+    public List<Feedback> getFeedbacksByAdvert(String advert_id);
+
+    public List<Feedback> getFeedbacksByAdvertWithComments(String advert_id, Integer num);
+
+    public List<Feedback> getFeedbacksByAdvertWithComments(String advert_id);
+
+    public List<Feedback> getFeedbacksByAdvertWithoutComments(String advert_id, Integer num);
+
+    public List<Feedback> getFeedbacksByAdvertWithoutComments(String advert_id);
 }
--- source/feedback_service/src/main/java/com/hirehawk/feedback_service/feedback/dao/FeedbackDAOImpl.java
+++ source/feedback_service/src/main/java/com/hirehawk/feedback_service/feedback/dao/FeedbackDAOImpl.java
@@ -21,7 +21,7 @@ public class FeedbackDAOImpl implements FeedbackDAO {
     private SessionFactory sessionFactory;
     //private static int i=0;
     @Override
-    public void saveFeedback(int mark, String comment, Date datetime, String userWhoLeft, String userAbout, USERROLE userAboutRole) {
+    public void saveFeedback(Integer mark, String comment, Date datetime, String userWhoLeft, String userAbout, USERROLE userAboutRole,String advert) {
         Feedback feedback = new Feedback();
         //feedback.setId(i);
        // i+=1;
@@ -31,19 +31,23 @@ public class FeedbackDAOImpl implements FeedbackDAO {
         feedback.setUserLeft(userWhoLeft);
         feedback.setUserAboutRole(userAboutRole.toString());
         feedback.setDate(datetime);
+        if (!(advert==null))
+            feedback.setAdvert(advert);
         Session session = this.sessionFactory.getCurrentSession();
         session.persist(feedback);
         session.flush();
     }
 
     @Override
-    public void saveFeedback(int mark, Date datetime, String userWhoLeft, String userAbout, USERROLE userAboutRole) {
+    public void saveFeedback(Integer mark, Date datetime, String userWhoLeft, String userAbout, USERROLE userAboutRole,String  advert) {
         Feedback feedback = new Feedback();
         feedback.setMark(mark);
         feedback.setUserAbout(userAbout);
         feedback.setUserLeft(userWhoLeft);
         feedback.setUserAboutRole(userAboutRole.toString());
         feedback.setDate(datetime);
+        if (!(advert==null))
+            feedback.setAdvert(advert);
 
         Session session = this.sessionFactory.getCurrentSession();
         session.persist(feedback);
@@ -62,7 +66,7 @@ public class FeedbackDAOImpl implements FeedbackDAO {
     }
 
     @Override
-    public List<Feedback> getFeedbacks(String user, USERROLE role, int num) {
+    public List<Feedback> getFeedbacks(String user, USERROLE role, Integer num) {
         Session session = this.sessionFactory.getCurrentSession();
         String sql = "SELECT f FROM " + Feedback.class.getName() + " f WHERE f.userAboutRole =:role";
         Query<Feedback> query = session.createQuery(sql, Feedback.class);
@@ -89,7 +93,7 @@ public class FeedbackDAOImpl implements FeedbackDAO {
     }
 
     @Override
-    public List<Feedback> getFeedbacksWithoutComments(String  user, USERROLE role, int num) {
+    public List<Feedback> getFeedbacksWithoutComments(String  user, USERROLE role, Integer num) {
         List<Feedback> resAll = getFeedbacks(user,role);
         List<Feedback> res = new ArrayList<Feedback>();
         for (int i = 0; i < resAll.size(); i++) {
@@ -102,6 +106,15 @@ public class FeedbackDAOImpl implements FeedbackDAO {
             res = res.subList(res.size() - num, res.size() - 1);
         }
         return res;
+    }
+
+
+    @Override
+    public void delFeedback(Integer id){
+        Session session = this.sessionFactory.getCurrentSession();
+        Feedback feedback = new Feedback();
+        feedback.setId(id);
+        session.delete(feedback);
     }
 
     @Override
@@ -118,7 +131,7 @@ public class FeedbackDAOImpl implements FeedbackDAO {
     }
 
     @Override
-    public List<Feedback> getFeedbacksWithComments(String  user, USERROLE role, int num) {
+    public List<Feedback> getFeedbacksWithComments(String  user, USERROLE role, Integer num) {
         List<Feedback> resAll = getFeedbacks(user,role);
         List<Feedback> res = new ArrayList<Feedback>();
         for (int i = 0; i < resAll.size(); i++) {
@@ -132,6 +145,91 @@ public class FeedbackDAOImpl implements FeedbackDAO {
         }
         return res;
     }
+
+
+    @Override
+    public List<Feedback> getFeedbacksByAdvert(String  advert_id) {
+        Session session = this.sessionFactory.getCurrentSession();
+        String sql = "SELECT f FROM " + Feedback.class.getName() + " f WHERE f.advert =:advert";
+        Query<Feedback> query = session.createQuery(sql, Feedback.class);
+        query.setParameter("advert", advert_id.toString());
+        List<Feedback> res = query.getResultList();
+        res.sort(new DateComparator());
+        return res;
+    }
+
+    @Override
+    public List<Feedback> getFeedbacksByAdvert(String advert_id , Integer num) {
+        Session session = this.sessionFactory.getCurrentSession();
+        String sql = "SELECT f FROM " + Feedback.class.getName() + " f WHERE f.advert =:advert";
+        Query<Feedback> query = session.createQuery(sql, Feedback.class);
+        query.setParameter("advert", advert_id.toString());
+        List<Feedback> res = query.getResultList();
+        res.sort(new DateComparator());
+        if (num < res.size()) {
+            res = res.subList(res.size() - num, res.size() - 1);
+        }
+        return res;
+    }
+
+    @Override
+    public List<Feedback> getFeedbacksByAdvertWithoutComments(String advert_id) {
+        List<Feedback> resAll = getFeedbacksByAdvert(advert_id);
+        List<Feedback> res = new ArrayList<Feedback>();
+        for (int i = 0; i < resAll.size(); i++) {
+            if (resAll.get(i).getComment() == null) {
+                res.add(resAll.get(i));
+            }
+        }
+        res.sort(new DateComparator());
+        return res;
+    }
+
+    @Override
+    public List<Feedback> getFeedbacksByAdvertWithoutComments(String  advert_id, Integer num) {
+        List<Feedback> resAll = getFeedbacksByAdvert(advert_id);
+        List<Feedback> res = new ArrayList<Feedback>();
+        for (int i = 0; i < resAll.size(); i++) {
+            if (resAll.get(i).getComment() == null) {
+                res.add(resAll.get(i));
+            }
+        }
+        res.sort(new DateComparator());
+        if (num < res.size()) {
+            res = res.subList(res.size() - num, res.size() - 1);
+        }
+        return res;
+    }
+
+    @Override
+    public List<Feedback> getFeedbacksByAdvertWithComments(String  advert_id) {
+        List<Feedback> resAll = getFeedbacksByAdvert(advert_id);
+        List<Feedback> res = new ArrayList<Feedback>();
+        for (int i = 0; i < resAll.size(); i++) {
+            if (resAll.get(i).getComment() != null) {
+                res.add(resAll.get(i));
+            }
+        }
+        res.sort(new DateComparator());
+        return res;
+    }
+
+    @Override
+    public List<Feedback> getFeedbacksByAdvertWithComments(String  advert_id, Integer num) {
+        List<Feedback> resAll = getFeedbacksByAdvert(advert_id);
+        List<Feedback> res = new ArrayList<Feedback>();
+        for (int i = 0; i < resAll.size(); i++) {
+            if (resAll.get(i).getComment() != null) {
+                res.add(resAll.get(i));
+            }
+        }
+        res.sort(new DateComparator());
+        if (num < res.size()) {
+            res = res.subList(res.size() - num, res.size() - 1);
+        }
+        return res;
+    }
+
 
     private class DateComparator implements Comparator<Feedback> {
 
